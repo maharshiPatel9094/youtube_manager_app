3885a5206773f692408bd6aef438638cd660f887
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-03-30 11:48:05 -0400

    added main def

--- youtube_manager.py
+++ youtube_manager.py
@@ -10,27 +10,32 @@ def update_video(videos):
 def delete_video(videos):
     pass        
 
-videos = []
+# entry point
+def main():
+    videos = []
+    while True:
+        print("\n Youtube Manager | choose an option ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video ")
+        print("3. Update a youtube video details ")
+        print("4. Delete a youtube video ")
+        print("5. Exit the app ")
+        choice = input("Enter your choice")
 
-while True:
-    print("\n Youtube Manager | choose an option ")
-    print("1. List all youtube videos ")
-    print("2. Add a youtube video ")
-    print("3. Update a youtube video details ")
-    print("4. Delete a youtube video ")
-    print("5. Exit the app ")
-    choice = input("Enter your choice")
+        match choice:
+            case "1":
+                list_all_videos(videos)
+            case "2":
+                add_video(videos)
+            case "3":
+                update_video(videos)
+            case "4":
+                delete_video(videos)
+            case "5":
+                break
+            case _:
+                print("Invalid Choice")      
 
-    match choice:
-        case "1":
-            list_all_videos(videos)
-        case "2":
-            add_video(videos)
-        case "3":
-            update_video(videos)
-        case "4":
-            delete_video(videos)
-        case "5":
-            break
-        case _:
-            print("Invalid Choice")       
+# checks weather the name of function is main
+if __name__ == "__main__":
+    main()
