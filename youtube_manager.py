fdfa3c85d16176d5d4e35284b98e20bca875fa76
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-03-30 12:13:38 -0400

    def update

--- youtube_manager.py
+++ youtube_manager.py
@@ -1,8 +1,31 @@
+import json
+
+
+def load_data():
+    try:
+        with open("youtube.txt","r") as file:   #r - read , w - write
+            # converts the file into json and load the data 
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+        
+# def for saving the videos and then load in file
+def save_data_helper(videos):
+    with open("youtube.txt", "w") as file:
+        json.dump(videos,file)
+        # dump() takes two parameters the data we want to write and where to write
+
 def list_all_videos(videos):
-    pass
+    # enumerate( ) adds indexing to the values
+    # indexing starts with 1
+    for index, video in enumerate(videos,start=1):
+        print(f"{index}. ")
 
 def add_video(videos):
-    pass
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({"name": name, "time": time})
+    save_data_helper(videos)
 
 def update_video(videos):
     pass
@@ -12,7 +35,7 @@ def delete_video(videos):
 
 # entry point
 def main():
-    videos = []
+    videos = load_data()
     while True:
         print("\n Youtube Manager | choose an option ")
         print("1. List all youtube videos ")
@@ -20,7 +43,7 @@ def main():
         print("3. Update a youtube video details ")
         print("4. Delete a youtube video ")
         print("5. Exit the app ")
-        choice = input("Enter your choice")
+        choice = input("Enter your choice ")
 
         match choice:
             case "1":
